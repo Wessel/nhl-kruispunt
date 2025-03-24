23555daf19c0858f9ce5402401378be6d252cfdc
Wessel Tip <wessel@go2it.eu>
2025-03-24 12:19:40 +0100

    fix(zeromq): Make it work with multiple machines

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,19 @@ import { ZmqSubscriber } from "./subscriber";
 import { ZmqPublisher } from "./publisher";
 
 const subscriber = new ZmqSubscriber()
-  .connect("tcp://127.0.0.1:5557")
-  .subscribe("topic", (topic, message) => {
+  .connect("tcp://192.168.56.243:5557")
+  .subscribe('python_test', (topic, message) => {
     console.log(`Received: [${topic}] ${message}`);
   })
   .bind();
 
 const publisher = new ZmqPublisher()
-  .bind("tcp://127.0.0.1:5557");
+  .bind("tcp://*:5557");
 
 setInterval(async() => {
   const message = `Message ${Math.random()}`;
   console.log(`Publishing: ${message}`);
-  publisher.send("topic", message);
+  publisher.send("python_test", message);
 }, 1000);
 
 process.on("SIGINT", async () => {
