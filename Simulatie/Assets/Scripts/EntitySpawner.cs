629ed67b97fc13310ab6d7c75936884c902fd82e
Glitch-coder <amberknobbe@gmail.com>
2025-05-01 21:34:55 +0200

    Spawnpoints gedefinieerd & roads toegevoegd aan entitySpawner

--- Simulatie/Assets/Scripts/EntitySpawner.cs
+++ Simulatie/Assets/Scripts/EntitySpawner.cs
@@ -5,10 +5,10 @@ public class EntitySpawner : MonoBehaviour
 {
   // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-  public GameObject entityPrefab;
+  public GameObject[] entityPrefabs;
   public Transform spawnPoint;
   public Transform parentTransform;
-
+  public Road[] availableRoads; 
   void Start()
   {
     SpawnEntity();
@@ -17,11 +17,21 @@ public class EntitySpawner : MonoBehaviour
     // Update is called once per frame
   void Update()
   {
-
+    
   }
 
   void SpawnEntity()
   {
-    GameObject newEntity = Instantiate(entityPrefab, spawnPoint.position, spawnPoint.rotation, parentTransform);
+    int randomIndex = Random.Range(0, entityPrefabs.Length);
+    GameObject newEntity = Instantiate(entityPrefabs[randomIndex], spawnPoint.position, spawnPoint.rotation, parentTransform);
+
+    // Verkrijg de MovingEntity component van de nieuw gespawnde entiteit
+    MovingEntity movingEntity = newEntity.GetComponent<MovingEntity>();
+    if (movingEntity != null && availableRoads.Length > 0)
+    {
+      // Kies een willekeurige weg uit de beschikbare wegen
+      Road randomRoad = availableRoads[Random.Range(0, availableRoads.Length)];
+      movingEntity.SetNewRoad(randomRoad); // Stel de weg in voor de MovingEntity
+    }
   }
 }
